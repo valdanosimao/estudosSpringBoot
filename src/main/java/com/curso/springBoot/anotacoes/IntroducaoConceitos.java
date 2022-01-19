abe8eef1d1611378d081028d672fb79ae785cf7f
Valdano Simão <valdanosimao@gmail.com>
2022-01-19 17:54:44 -0300

    Organizando os estudos

---
+++ src/main/java/com/curso/springBoot/anotacoes/IntroducaoConceitos.java
@@ -0,0 +1,54 @@
+package com.curso.springBoot.anotacoes;
+
+public class IntroducaoConceitos {
+	
+	/*  INJEÇÃO DE DEPEDÊNCIAS!
+	 * 	
+	 * 	É um padrão de projeto que já está implementado pelo Spring
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+
+}
